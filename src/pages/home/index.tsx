44c4351092bff5cee34766dbf5f49d623873dc46
Rabithua <34543831+Rabithua@users.noreply.github.com>
2023-07-10 18:16:24 +0800

    Initialize React-Typescript-Tailwind-ReactRouter-ReactHotToast

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <div className=" h-screen w-full flex justify-center items-center">
+        <h1 className="text-3xl mx-4 font-mono font-thin text-emerald-600">
+          React + Typescript + Tailwind + React router + React hot toast Here💫
+          <div className=" text-sm text-center my-4 text-gray-400">🤖 JUST CHANGE ./src/pages/home/index.tsx AND START YOUR MASTERPIECE</div>
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
