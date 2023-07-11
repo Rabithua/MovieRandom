61c08bb368e2bea781b7c5db8e7c1e65717f3c72
Rabithua <rabithua@gmail.com>
2023-07-11 16:22:24 +0800

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 const env = {
-    apiUrl: process.env.NODE_ENV === 'production' ? 'https://givemeamovie.cn/api' : 'http://localhost:5004',
+    apiUrl: process.env.NODE_ENV === 'production' ? 'https://api.givemeamovie.cn' : 'http://localhost:5004',
     debug: false
 };
 
